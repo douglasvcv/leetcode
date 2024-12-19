1c5463aae6c79ade4ff53eb721400347bccbc9e2
douglasvcv <douglasvitor1233@gmail.com>
2024-12-18 23:37:27 -0300

    add incomplete problem

---
+++ chalengers/1732. Find the Highest Altitude.js
@@ -0,0 +1,12 @@
+var largestAltitude = function(gain) {
+    let topNumber=0
+    gain.unshift(0)
+    for(let i = 0; i<gain.length; i++){
+        if(gain[i]>topNumber || gain[i]==topNumber){
+            topNumber=i
+        }
+    }
+    return topNumber
+};
+
+//gain is array that contain the numbers, i have return the high number inside gain
